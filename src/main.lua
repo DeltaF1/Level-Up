ae4fc8c2f86485cf9edcb1af01684f95f370af79
DeltaF1 <delyaerricson@gmail.com>
2016-08-09 20:50:23 -0400

    test

--- src/main.lua
+++ src/main.lua
@@ -3,4 +3,15 @@ Class = require "hump.class"
 
 function love.load()
 	print("test")
+	states = {}
+
+	for _, name in ipairs(love.filesystem.getDirectoryItems("states")) do
+		print(name)
+		_, _, name = name:find("(.*)%..*")
+		print(name)
+		states[name] = require("states."..name)
+	end
+
+	Gamestate.registerEvents()
+	Gamestate.switch(states.game) --Replace with states.intro
 end
---
+++ src/states/game.lua
@@ -0,0 +1,8 @@
+local game = {}
+
+function game:draw()
+	love.graphics.setColor(0, 50, 200)
+	love.graphics.print("TEST", 50, 50)
+end
+
+return game
